8cf164c7372e7396d45739c6714ce3ce94776a2d
elibdev <eli@elib.dev>
2021-06-06 16:43:41 -0400

    add basic exercise structure for rational numbers

---
+++ exercises/practice/rational-numbers/lib/rational_numbers.ex
@@ -0,0 +1,18 @@
+defmodule RationalNumbers do
+  @moduledoc """
+  Documentation for `RationalNumbers`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RationalNumbers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ exercises/practice/rational-numbers/test/rational_numbers_test.exs
@@ -0,0 +1,8 @@
+defmodule RationalNumbersTest do
+  use ExUnit.Case
+  doctest RationalNumbers
+
+  test "greets the world" do
+    assert RationalNumbers.hello() == :world
+  end
+end
