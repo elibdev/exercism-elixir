c9e330d28e2cec6f6d977e91cab10b4f8c9f18c6
elibdev <eli@elib.dev>
2021-06-10 03:00:14 -0400

    added implementation for multiplication without reducing rationals

--- exercises/practice/rational-numbers/lib/rational_numbers.ex
+++ exercises/practice/rational-numbers/lib/rational_numbers.ex
@@ -7,6 +7,12 @@ defmodule RationalNumbers do
     Enum.map(r, fn n -> abs(n) end)
   end
 
+  def calculate("mul", [r1, r2]) do
+    Enum.map([0, 1], fn i ->
+      Enum.at(r1, i) * Enum.at(r2, i)
+    end)
+  end
+
   def calculate(_op, r) do
     r
   end
--- exercises/practice/rational-numbers/test/rational_numbers_test.exs
+++ exercises/practice/rational-numbers/test/rational_numbers_test.exs
@@ -15,21 +15,34 @@ defmodule RationalNumbersTest do
                 group_desc = test_case["description"]
 
                 for test_case <- test_cases do
-                  test group_desc <> " > " <> test_case["description"] do
-                    assert false
+                  case test_case do
+                    %{"property" => "mul"} = test_case ->
+                      test test_case["description"] <>
+                             " > mul #{Jason.encode!(test_case["input"])}" do
+                        r1 = unquote(test_case["input"]["r1"])
+                        r2 = unquote(test_case["input"]["r2"])
+                        expected = unquote(test_case["expected"])
+                        assert RationalNumbers.calculate("mul", [r1, r2]) == expected
+                      end
+
+                    _ ->
+                      test group_desc <> " > " <> test_case["description"] do
+                        assert unquote(test_case["property"]) ==
+                                 unquote(Map.keys(test_case["input"]))
+                      end
                   end
                 end
 
               %{"property" => "abs"} = test_case ->
                 test test_case["description"] do
-                  input = unquote(test_case["input"]["r"])
+                  r = unquote(test_case["input"]["r"])
                   expected = unquote(test_case["expected"])
-                  assert RationalNumbers.calculate("abs", input) == expected
+                  assert RationalNumbers.calculate("abs", r) == expected
                 end
 
               _ ->
                 test test_case["description"] do
-                  assert false
+                  assert unquote(test_case["property"]) == unquote(Map.keys(test_case["input"]))
                 end
             end
           end
